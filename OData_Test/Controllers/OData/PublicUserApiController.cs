5ab256d923dfa93f876a36ca59398f8d95d315d8
vnmatt <gordon_matt@hotmail.com>
2017-12-14 09:27:46 +1030

    First commit

---
+++ OData_Test/Controllers/OData/PublicUserApiController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Query;
+using OData_Test.Data;
+
+namespace OData_Test.Controllers.OData
+{
+    public class PublicUserApiController : ODataController
+    {
+        private readonly ApplicationDbContext context;
+
+        public PublicUserApiController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        //[EnableQuery(AllowedQueryOptions = AllowedQueryOptions.All)]
+        public virtual IEnumerable<PublicUserInfo> Get(ODataQueryOptions<PublicUserInfo> options)
+        {
+            var settings = new ODataValidationSettings()
+            {
+                AllowedQueryOptions = AllowedQueryOptions.All
+            };
+            options.Validate(settings);
+
+            var query = context.Users
+                .Select(x => new PublicUserInfo
+                {
+                    Id = x.Id,
+                    UserName = x.UserName
+                });
+
+            var results = options.ApplyTo(query);
+            return (results as IQueryable<PublicUserInfo>).ToHashSet();
+        }
+
+        public virtual async Task<PublicUserInfo> Get([FromODataUri] string key)
+        {
+            var entity = await context.Users.FindAsync(key);
+            return new PublicUserInfo
+            {
+                Id = entity.Id,
+                UserName = entity.UserName
+            };
+        }
+    }
+
+    public class PublicUserInfo
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ OData_Test/Startup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
+using OData_Test.Controllers.OData;
+using OData_Test.Data;
+using OData_Test.Models;
+using OData_Test.Services;
+
+namespace OData_Test
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddOData();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            var edmModel = GetEdmModel(app.ApplicationServices);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapODataServiceRoute("OData", "odata", edmModel);
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private static IEdmModel GetEdmModel(IServiceProvider serviceProvider)
+        {
+            var builder = new ODataConventionModelBuilder(serviceProvider);
+
+            builder.EntitySet<PublicUserInfo>("PublicUserApi");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
